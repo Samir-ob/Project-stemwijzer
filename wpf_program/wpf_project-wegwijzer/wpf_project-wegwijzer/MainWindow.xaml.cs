c8882163539df888a7a7439e69a2a63a63a68b8f
samir_ob <PS262209@edu.summacollege.nl>
2025-05-15 13:33:46 +0200

    Wpf program

---
+++ wpf_program/wpf_project-wegwijzer/wpf_project-wegwijzer/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_project_wegwijzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClosingAllPages()
+        {
+            grid_NieuwsPagina.Visibility = Visibility.Hidden;
+            grid_StandpuntPagina.Visibility = Visibility.Hidden;
+        }
+        private void btn_NieuwsPagina_Click(object sender, RoutedEventArgs e)
+        {
+            ClosingAllPages();
+            grid_NieuwsPagina.Visibility = Visibility.Visible;
+        }
+
+        private void btn_StandpuntPagina_Click(object sender, RoutedEventArgs e)
+        {
+            ClosingAllPages();
+            grid_StandpuntPagina.Visibility = Visibility.Visible;
+        }
+    }
+}
